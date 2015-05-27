d7e9dc8e6d7c08bb49ad4c64844dc4895e2c28cf
Aaron Keys <aaron.s.keys@gmail.com>
2015-05-27 10:40:54 -0700

    add timeseries analysis submodule placeholder

---
+++ contrib/services/stat_daemon/stat_daemon/tsa.py
@@ -0,0 +1,59 @@
+import numpy as np
+import math
+
+def deviation_raw(x, y):
+    """
+    Quick and dirty absolute error
+    """
+    return [math.fabs(xi-yi) for xi, yi in zip(x, y)]
+
+def deviation_normalized(x, y):
+    """
+    Quick and dirty normalized rms error for now
+    """
+    return [(xi-yi)*(xi-yi)/(xi*yi) for xi, yi in zip(x, y)]
+
+def deviation(x, y):
+    """
+    Attempt to return a reasonable deviation.  When the data is normalized
+    correctly, we shouldn't need this
+    """
+    if all([i >= 1.0 for i in x]):
+        return deviation_normalized(x, y)
+    else:
+        return deviation_raw(x, y)
+
+def detrend(df):
+    """
+    Takes a pandas dataframe and attempts to detrend
+
+    ** Note ** this is just a placeholder for an actual detrending
+    function to be filled in later.
+    """
+    y0 = df[df.columns[0]]
+    unit = np.array([1.0 for i in y0])
+    y = []
+    delta = []
+    # polynomial fit (overfit for now)
+    x = range(len(y0))
+    p = np.polyfit(x, y0, 6)
+    poly = np.poly1d(p)
+    yp = [poly(i) for i in x]
+    y.append(yp)
+    delta.append(deviation(y0, yp))
+    # fft fit
+    rft = np.fft.rfft(y0)
+    for i in range(0, len(rft)):
+        if math.fabs(rft[i]) < np.mean(y0):
+            rft[i] = 0.0
+    yfft = np.fft.irfft(rft, n=len(y0))
+    y.append(yfft)
+    delta.append(deviation(y0, yfft))
+    # fit polynomial to wow and yoy growth rates
+    # extrapolate each data point from previous week and year
+    dd = [sum(d) for d in delta]
+    print dd
+    idx = np.argmin(dd)
+    df['Trend'] = y[idx]
+    df['Residuals'] = delta[idx]
+    return df
